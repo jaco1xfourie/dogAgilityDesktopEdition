2098a1fd71ac5f90a3fec698b939854e582de02f
Jaco Fourie <jaco1xfourie@gmail.com>
2020-04-15 16:22:30 +0200

    removed some code from main - just to test git function

--- src/AgilityMain.java
+++ src/AgilityMain.java
@@ -2,11 +2,7 @@ public class AgilityMain {
 
     public static void main(String[] args) {
 
-        Handler jaco = new Handler();
 
-        jaco.setName("Jaco");
-
-        System.out.println(jaco.getName());
     }
 
 }
